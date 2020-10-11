9f0232475a32879425bbaa74b538037a3c4bddde
Stewart Lynch <slynch@createchsol.com>
2020-10-11 15:50:38 -0700

    Initial Commit

---
+++ ButtonStyling/ButtonStylingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStylingApp.swift
+//  ButtonStyling
+//
+//  Created by Stewart Lynch on 2020-10-11.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStylingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonStyling/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  ButtonStyling
+//
+//  Created by Stewart Lynch on 2020-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Sample 1") {
+                print("Tapped")
+            }.buttonStyle(FilledRoundedCornerButtonStyle())
+            Button("Sample 2") {
+                
+            }.buttonStyle(FilledRoundedCornerButtonStyle(font: .body,  bgColor: .red, fgColor: .yellow))
+            HStack {
+                Button("Delete") {
+                    
+                }.buttonStyle(SpecialButtonStyle(actionType: .delete))
+                Spacer()
+                Button("Cancel") {
+                    
+                }.buttonStyle(SpecialButtonStyle(actionType: .cancel))
+                Button("OK") {
+                    
+                }.buttonStyle(SpecialButtonStyle(actionType: .confirm))
+            }.padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
